9e47402dab73e0ab681a4c9a3012690108c54716
TOMORI-TETSUYA <x20a_zgmf_strike@yahoo.co.jp>
2025-09-16 14:15:31 +0900

    星を描く まで

--- red.py
+++ red.py
@@ -15,3 +15,15 @@ game_complete = False
 current_level = 1
 stars =[]
 animations = []
+
+def draw():
+    global stars, current_level, game_over, game_complete
+    screen.clear()
+    screen.blit("space", (0, 0))
+    if geme_over:
+        display_message("GAME OVER!", "Try again.")
+    elif game_complete:
+        display_message("YOU WON!", "well done.")
+    else:
+        for star in stars:
+            star.draw()
